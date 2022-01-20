0bffbe3999eed96207353d0d4d8a4a14c735cc29
Bwnr-Pwnr <zaxdev59@gmail.com>
2022-01-20 15:22:56 -0700

    Got the stuff to work on Fran's PC and thin out some errors

--- docx-test.py
+++ docx-test.py
@@ -1,9 +1,10 @@
-import pandas as pd, re, os, PyPDF2 as pdf
+from code import interact
+from importlib.metadata import files
+import re, os
 from time import sleep
 from docx import Document
-from colorama import Style, Fore
 from datetime import date as dt
-from docx2pdf import convert
+from phaxio import PhaxioApi
 
 # convert('./medrecs/faxcover.docx')
 
@@ -25,14 +26,17 @@ def docx_replace_regex(doc_obj, regex , replace):
 
 # print(patient['yourName'])
 def main():
-    pwChoices = int(input(f'Please select additional docs\nThe fax cover will be made regardless\n1. Anticoagulant\n2. Pacemaker\n3. Both\nSelection: '))
+    import PyPDF2 as pdf    
+    pwChoices = int(input(f'Please select additional docs\nThe fax cover will be made regardless\n1. Both                  2. Pacemaker\n3. Anticoagulant         4. Faxcover only\nSelection: '))
     print(f'You chose: {pwChoices}')
     if (pwChoices == 1):
         docs = [Document('./medrecs/faxcover.docx'), Document('./medrecs/Anticoagulantform.docx'), Document('./medrecs/pacemakerform.docx')]
     elif (pwChoices == 2):
         docs = [Document('./medrecs/faxcover.docx'), Document('./medrecs/pacemakerform.docx')]
     elif (pwChoices == 3):
-        docs = [Document('./medrecs/faxcover.docx'), Document('./medrecs/Anticoagulantform.docx'), Document('./medrecs/pacemakerform.docx')]
+        docs = [Document('./medrecs/faxcover.docx'), Document('./medrecs/Anticoagulantform.docx')]
+    elif (pwChoices == 4):
+        docs = [Document('./medrecs/faxcover.docx')]
     else:
         print('Please select a valid option')
         sleep(2)
@@ -45,16 +49,16 @@ def main():
     "yourName": 'Zackery H./Franchesca G.',
     "dateOfFax": dt.today().strftime("%B %d, %Y"),
     "urgency": 'URGENT',
-    "numberOfPages": input(f'How many {Fore.YELLOW}pages{Style.RESET_ALL} are being faxed?\n'),
-    "fNumber": input(f'What is the number you are {Fore.RED}faxing to{Style.RESET_ALL}?\n'),
-    "pNumber": input(f'What is the {Fore.RED}phone number{Style.RESET_ALL} of the facility you are faxing?\n'),
-    "drName": input(f'What is the name of the {Fore.RED}doctor you are contacting{Style.RESET_ALL}? (Name only! No "Dr." needed!)\n'),
-    "ptName": input(f'What is the name of the {Fore.RED}patient{Style.RESET_ALL}?\n'),
-    "dateOfBirth": input(f"What is the patient's {Fore.RED}date of birth{Style.RESET_ALL}?\n"),
-    "procedureName": input(f'What is the {Fore.RED}procedure name{Style.RESET_ALL}? (i.e. PVP (Photovaporization of the prostate))\n'),
-    "procedureDate": input(f'What is the {Fore.RED}date{Style.RESET_ALL} of the procedure?\n'),
-    "anesthesiologistName": input(f'Who is the {Fore.RED}surgeon{Style.RESET_ALL}? (Kaplan, Wong...)\n')
-}
+    "numberOfPages": input(f'How many pages are being faxed?\n'),
+    "fNumber": input(f'What is the number you are faxing to?\n'),
+    "pNumber": input(f'What is the phone number of the facility you are faxing?\n'),
+    "drName": input(f'What is the name of the doctor you are contacting? (Name only! No "Dr." needed!)\n'),
+    "ptName": input(f'What is the name of the patient?\n'),
+    "dateOfBirth": input(f"What is the patient's date of birth?\n"),
+    "procedureName": input(f'What is the procedure name? (i.e. PVP (Photovaporization of the prostate))\n'),
+    "procedureDate": input(f'What is the date of the procedure?\n'),
+    "anesthesiologistName": input(f'Who is the surgeon? (Kaplan, Wong...)\n')
+    }
 
     # Input data into tables
     for doc in docs:
@@ -75,13 +79,53 @@ def main():
     for doc in docs:
         if (x == 0):
             doc.save(f"faxcover_{patient['ptName']}.docx")
-        elif (x == 1): 
+        elif (x == 1 and pwChoices == 2): 
+            doc.save(f"pacemaker_{patient['ptName']}.docx")
+        elif (x == 1 and pwChoices != 2):
             doc.save(f"anticoag_{patient['ptName']}.docx")
-            
+        elif (x == 2 and pwChoices == 1):
+            doc.save(f"pacemaker_{patient['ptName']}.docx")
         x += 1
-        print(x)
     # Edit files to be PDFs
     os.system('powershell docx2pdf .')
+    sleep(2)
     mergeFile = pdf.PdfFileMerger()
-    pdfFiles = [pdf.PdfFileReader(f"faxcover_{patient['ptName']}.docx")]
+    if (pwChoices == 1):
+        pdfFiles = [pdf.PdfFileReader(f'faxcover_{patient["ptName"]}.pdf', 'rb'), pdf.PdfFileReader(f'anticoag_{patient["ptName"]}.pdf', 'rb'), pdf.PdfFileReader(f'pacemaker_{patient["ptName"]}.pdf', 'rb')]
+    elif (pwChoices == 2):
+        pdfFiles = [pdf.PdfFileReader(f'faxcover_{patient["ptName"]}.pdf', 'rb'), pdf.PdfFileReader(f'pacemaker_{patient["ptName"]}.pdf', 'rb')]
+    elif (pwChoices == 3):
+        pdfFiles = [pdf.PdfFileReader(f'faxcover_{patient["ptName"]}.pdf', 'rb'), pdf.PdfFileReader(f'anticoag_{patient["ptName"]}.pdf', 'rb')]
+    elif (pwChoices == 4):
+        pdfFiles = [pdf.PdfFileReader(f'faxcover_{patient["ptName"]}.pdf', 'rb')]
+    for pdf in pdfFiles:
+        mergeFile.append(pdf)
+    names = patient['ptName'].split(' ')
+    os.system('powershell rm *.docx')
+    os.system('powershell rm *.pdf')
+    mergeFile.write(f'Request- {names[1]}, {names[0]} med recs req.pdf')
+    file2Fax = f"Request- {names[1]}, {names[0]} med recs req.pdf"
+    print(type(str(file2Fax)))
+
+    def faxIt(pt):
+        fileFaxing = f"Request- {names[1]}, {names[0]} med recs req.pdf"
+        phaxio = PhaxioApi('326hm7hwwjgtnh18qo66lauwmx4oc8wf2vbth6he', '014a3atyonq17kx4ftixk6orfs0addix5yowk9jd')
+        phaxio.Fax.send(
+            to='6233221504',
+            files=fileFaxing
+        )
+    faxIt(patient)
+    os.system(r"powershell mv *.pdf 'S:\'")
+
+
+
+    return patient
 main()
+
+
+
+
+
+
+
+
---
+++ test.ps1
@@ -0,0 +1,1 @@
+& "C:\Users\OSC\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\OSC\Desktop\Python\docx-test.py"
