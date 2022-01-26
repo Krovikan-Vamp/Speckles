0750c2ee3f7231de70bd9f0477e41624cc795b36
Bwnr-Pwnr <zaxdev59@gmail.com>
2022-01-26 09:20:13 -0700

    Added some QoL changes to 'docx-test.py'

--- aojd.py
+++
@@ -1,6 +0,0 @@
-mylist = ['1', '2', '3']
-
-try:
-     mylist.index('9')
-except ValueError:
-    print('Yep, its a ValueError')
--- docx-test.py
+++ docx-test.py
@@ -1,25 +1,17 @@
-import re, os, openpyxl as oxl
+import re
+import os
+import openpyxl as oxl
 from time import sleep
 from docx import Document
 from datetime import date as dt
 import inquirer
 from tqdm import tqdm
-# Clearance log
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-cred = credentials.Certificate("./sa.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-# from phaxio import PhaxioApi
-
-# convert('./medrecs/faxcover.docx')
 
 # Search the doc(s) and replace data
-def docx_replace_regex(doc_obj, regex , replace):
-    
+
+
+def docx_replace_regex(doc_obj, regex, replace):
+
     for p in doc_obj.paragraphs:
         if regex.search(p.text):
             inline = p.runs
@@ -32,14 +24,17 @@ def docx_replace_regex(doc_obj, regex , replace):
     for table in doc_obj.tables:
         for row in table.rows:
             for cell in row.cells:
-                docx_replace_regex(cell, regex , replace)
+                docx_replace_regex(cell, regex, replace)
 
 # print(patient['yourName'])
+
+
 def main():
     os.system('cls')
     import PyPDF2 as pdf
 
-    doc_question = [inquirer.Checkbox('docs', message=f'What documents do you need?', choices=['Anticoagulant', 'A1c Tests', 'Pacemaker', 'Clearance'])]
+    doc_question = [inquirer.Checkbox('docs', message=f'What documents do you need?', choices=[
+                                      'Anticoagulant', 'A1c Tests', 'Pacemaker', 'Clearance'])]
     prompt = inquirer.prompt(doc_question)
     print(prompt['docs'])
     raw_docs = []
@@ -56,19 +51,19 @@ def main():
     # print(f"The new docs are: {docs}")
     prompt['docs'].append('Faxcover')
     patient = {
-    "ptName": input(f'What is the name of the patient?\n'),
-    "dateOfBirth": input(f"What is the patient's date of birth?\n"),
-    "procedureDate": input(f'What is the date of the procedure?\n'),
-    "procedureName": input(f'What is the procedure name? (i.e. PVP (Photovaporization of the prostate))\n'),
-    "anesthesiologistName": input(f'Who is the surgeon? (Kaplan, Wong...)\n'),
-    "drName": input(f'What is the name of the doctor you are contacting? (Name only! No "Dr." needed!)\n'),
-    "pNumber": input(f'What is the phone number of the facility you are faxing?\n'),
-    "fNumber": input(f'What is the number you are faxing to?\n'),
-    "forms": prompt["docs"],
-    "dateOfFax": dt.today().strftime("%B %d, %Y"),
-    "numberOfPages": str(len(docs)),
-    "urgency": 'URGENT',
-    "yourName": 'Zackery H./Franchesca G.',
+        "ptName": input(f'What is the name of the patient?\n'),
+        "dateOfBirth": input(f"What is the patient's date of birth?\n"),
+        "procedureDate": input(f'What is the date of the procedure?\n'),
+        "procedureName": input(f'What is the procedure name? (i.e. PVP (Photovaporization of the prostate))\n'),
+        "anesthesiologistName": input(f'Who is the surgeon? (Kaplan, Wong...)\n'),
+        "drName": input(f'What is the name of the doctor you are contacting? (Name only! No "Dr." needed!)\n'),
+        "pNumber": input(f'What is the phone number of the facility you are faxing?\n'),
+        "fNumber": input(f'What is the number you are faxing to?\n'),
+        "forms": prompt["docs"],
+        "dateOfFax": dt.today().strftime("%B %d, %Y"),
+        "numberOfPages": str(len(docs)),
+        "urgency": 'URGENT',
+        "yourName": 'Zackery H./Franchesca G.',
     }
     os.system('cls')
     # Input data into tables
@@ -79,23 +74,23 @@ def main():
                 for cell in row.cells:
                     for paragraph in cell.paragraphs:
                         for word, replacement in patient.items():
-                            word_re=re.compile(word)
-                            docx_replace_regex(doc, word_re , replacement)
+                            word_re = re.compile(word)
+                            docx_replace_regex(doc, word_re, replacement)
     # Input data into paragraphs
     for doc in docs:
         for word, replacement in patient.items():
-            word_re=re.compile(word)
-            docx_replace_regex(doc, word_re , replacement)
+            word_re = re.compile(word)
+            docx_replace_regex(doc, word_re, replacement)
     # Save the changed files "as"
     for i in tqdm(range(50), desc="Applying info to templates", ascii=True):
         sleep(0.01)
     x = 0
     for doc in docs:
         doc.save(f"file_{x}.docx")
-            # doc.save(f"pacemaker_{patient['ptName']}.docx")
+        # doc.save(f"pacemaker_{patient['ptName']}.docx")
         if docs.index(doc) != len(docs) - 1:
             x += 1
-            
+
     # Edit files to be PDFs
     print('Converting to PDFs...')
     os.system('powershell docx2pdf .')
@@ -113,12 +108,12 @@ def main():
     os.system('powershell rm *.docx')
     os.system('powershell rm *.pdf')
     mergeFile.write(f'Request- {names[1]}, {names[0]} Med Recs Req.pdf')
-    
-    # Write to excel
-    wb = oxl.Workbook() # Create workbook
-    ws = wb.active # Set active worksheet
 
-    col = 1 
+    # Write to excel
+    wb = oxl.Workbook()  # Create workbook
+    ws = wb.active  # Set active worksheet
+
+    col = 1
     igs = ['urgency', 'yourName', 'numberOfPages', 'fNumber']
     patient['forms'] = ', '.join(patient['forms'])
     patient['pNumber'] += '/' + patient['fNumber']
@@ -126,36 +121,29 @@ def main():
     patient['pNumber'] = patient['pNumber'].replace('-', '.')
     for key, value in patient.items():
         try:
-            igs.index(key) # See if key is ignored
+            igs.index(key)  # See if key is ignored
         except ValueError:
-            ws.cell(1, col, value) # Write the data
+            ws.cell(1, col, value)  # Write the data
             col += 1
         pass
     wb.save(f'Request- {names[1]}, {names[0]} Med Recs Req.xlsx')
 
+    # Move patient to the scans folder
     print(f'Adding {patient["ptName"]}...')
     os.system(r"powershell mv *.pdf 'S:\'")
-    # db.collection('Patient Clearance').document(f"{patient['ptName']} ({patient['dateOfBirth']})").set(patient)
-
-
-    print('Done!')
-    sleep(1)
+    print("Don't forget to add them to the log! Check the new '.xlsx' file!")
+    sleep(5)
     os.system('cls')
     return True
+
+
 main()
 
-
-
-
-
-
-
-
- # def faxIt(pt):
-    #     fileFaxing = f"Request- {names[1]}, {names[0]} med recs req.pdf"
-    #     phaxio = PhaxioApi('326hm7hwwjgtnh18qo66lauwmx4oc8wf2vbth6he', '014a3atyonq17kx4ftixk6orfs0addix5yowk9jd')
-    #     phaxio.Fax.send(
-    #         to='6233221504',
-    #         files=fileFaxing
-    #     )
-    # faxIt(patient)
+# def faxIt(pt):
+#     fileFaxing = f"Request- {names[1]}, {names[0]} med recs req.pdf"
+#     phaxio = PhaxioApi('326hm7hwwjgtnh18qo66lauwmx4oc8wf2vbth6he', '014a3atyonq17kx4ftixk6orfs0addix5yowk9jd')
+#     phaxio.Fax.send(
+#         to='6233221504',
+#         files=fileFaxing
+#     )
+# faxIt(patient)
